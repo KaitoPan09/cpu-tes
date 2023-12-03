fb53f6d829f6048cf8befa1f1386617e079738b2
KaitoPan09 <93980610+KaitoPan09@users.noreply.github.com>
2023-12-03 19:46:05 +0800

    wip

--- src/pages/questionnaire/index.jsx
+++ src/pages/questionnaire/index.jsx
@@ -53,132 +53,7 @@ const Questionnaire = () => {
         setOpenAddDialog(true);
     };
     const [rows, setRows] = React.useState(dummyQuestionnaireIndex);
-
-    //hidden comments datagrid
-    const [showFeedback, setShowFeedback] = useState(false);
-
-    //dialogbox
-    // const [openDialog, setOpenDialog] = useState(false);
-    const [selectedCategory, setSelectedCategory] = useState(null);
-
-    //edit rows
-    const [rowModesModel, setRowModesModel] = React.useState({});
-
-    //delete rows
-    const [deleteId, setDeleteId] = useState(null);
-    const [open, setOpen] = React.useState(false);
-
-    const handleRowEditStop = (params, event) => {
-        if (params.reason === GridRowEditStopReasons.rowFocusOut) {
-            event.defaultMuiPrevented = true;
-        }
-    };
     
-    const handleEditClick = (id) => () => {
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-    };
-    
-    const handleSaveClick = (id) => () => {
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-    };
-    
-    const handleDeleteClick = (id) => () => {
-        setDeleteId(id);
-        setOpen(true);
-    };
-
-    const handleDeleteConfirm = () => {
-        setRows(rows.filter((row) => row.id !== deleteId));
-        setOpen(false);
-    }
-
-    const handleDeleteCancel =() => {
-        setOpen(false);
-    }
-    
-    const handleCancelClick = (id) => () => {
-        setRowModesModel({
-            ...rowModesModel,
-            [id]: { mode: GridRowModes.View, ignoreModifications: true },
-    });
-    
-    const editedRow = rows.find((row) => row.id === id);
-        if (editedRow.isNew) {
-            setRows(rows.filter((row) => row.id !== id));
-        }
-    };
-    
-    const processRowUpdate = (newRow) => {
-        const updatedRow = { ...newRow, isNew: false };
-        setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
-        return updatedRow;
-    };
-    
-    const handleRowModesModelChange = (newRowModesModel) => {
-        setRowModesModel(newRowModesModel);
-    };
-
-    const handleRowClick = (params) => {
-        setSelectedCategory(params.row.category);
-    }
-
-    const filteredDummyQuestionnaireQN = dummyQuestionnaireQN.filter(
-        (item) => item.category === selectedCategory
-    );
-
-    const handleFeedbackClick = () => {
-        setShowFeedback((prevShowFeedback) => !prevShowFeedback);
-    }
-
-    //questionType combobox
-    const questionType = ['Yes/No', 'Text'];
-
-    const QuestionToolbar = () => {
-        return (
-            <GridToolbarContainer>
-                <Button 
-                    startIcon={<AddIcon />}
-                    sx={{
-                        padding: "4px 0px",
-                        fontSize: "0.6964285714285714rem"
-                    }}
-                    >
-                    ADD QUESTION
-                </Button>
-                <GridToolbarColumnsButton />
-                <GridToolbarFilterButton />
-                <Button
-                    startIcon={showFeedback ? <PollOutlinedIcon/> 
-                    : <TextsmsOutlinedIcon/>}
-                    sx={{
-                        padding: "4px 0px",
-                        fontSize: "0.6964285714285714rem"
-                    }}
-                    onClick={handleFeedbackClick}
-                    >
-                    {showFeedback ? "SURVEY" : "FEEDBACK"}
-                </Button>
-            </GridToolbarContainer>
-        )
-    }
-
-    const CategoryToolbar = () => {
-        return (
-            <GridToolbarContainer>
-                <Button 
-                    startIcon={<AddIcon />}
-                    sx={{
-                        padding: "4px 0px",
-                        fontSize: "0.6964285714285714rem"
-                    }}
-                    >
-                    ADD QUESTION
-                </Button>
-                <GridToolbarFilterButton />
-            </GridToolbarContainer>
-        )
-    }   
-
     const columns = [
         { 
             field: "created", 
@@ -217,26 +92,32 @@ const Questionnaire = () => {
             flex: 1,
             renderCell: ({ value }) => {
                 const iconStyle = { fontSize: '1.25rem' };
-                const icon = value === "Active"
-                    ? <CheckIcon 
-                        sx={{ 
-                            fontSize: iconStyle.fontSize, 
-                            color: colors.greenAccent[500] 
-                        }}/>
-                    : <WarningAmberIcon 
-                        sx={{ 
-                            fontSize: iconStyle.fontSize, 
-                            color: colors.redAccent[500] 
-                        }}/>
-                const color = value === "Active" ? colors.greenAccent[500]
-                    : colors.redAccent[500]
                 return [
-                <Tooltip title={`Status: ${value}`}>
-                    <IconButton>
-                        {icon}
-                        <Typography sx={{ color }}>{value}</Typography>
-                    </IconButton>
-                </Tooltip>,
+                    value === "Active" ? 
+                        <Tooltip title={`Status: Active`}>
+                            <IconButton>
+                                <CheckIcon 
+                                    sx={{ 
+                                        fontSize: iconStyle.fontSize, 
+                                        color: colors.greenAccent[300] 
+                                    }}/>
+                                <Typography sx={{ color: colors.greenAccent[300] }}>
+                                    Active
+                                </Typography>
+                            </IconButton>
+                        </Tooltip>
+                        : <Tooltip title={`Click to Activate Questionnaire`}>
+                            <IconButton>
+                                <WarningAmberIcon 
+                                    sx={{ 
+                                        fontSize: iconStyle.fontSize, 
+                                        color: colors.redAccent[500] 
+                                    }}/>
+                                <Typography sx={{ color: colors.redAccent[500] }}>
+                                    Inactive
+                                </Typography>
+                            </IconButton>
+                        </Tooltip>
                 ];
             },
         },
@@ -274,7 +155,6 @@ const Questionnaire = () => {
                     columns={columns} 
                     handleAdd={handleAdd}
                     btnText={"ADD NEW QUESTIONNAIRE"}
-                    onRowClick={handleRowClick}
                     />
                 <FormDialog
                     setRows={setRows}
